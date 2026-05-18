a6d0f3960be898e4000991eccc836f8747dabcd0
Tudor-tech <tudor.slavu@ulbsibiu.ro>
2026-05-18 10:40:58 +0300

    forEach method

--- src/main/java/org/parkinglot/Main.java
+++ src/main/java/org/parkinglot/Main.java
@@ -1,13 +1,13 @@
 package org.parkinglot;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     static void main() {
-        List<Integer> n = getNumbers();
-        List<Integer> even =  filterEvenNumbers(n);
-        List<Integer> odd =  filterOddNumbers(n);
-        System.out.println(odd);
+        List<Integer> numbers = getNumbers();
+        numbers.stream().forEach(number -> System.out.println(number)) ;
     }
 
     public static List<Integer> getNumbers() {
@@ -16,25 +16,5 @@ public class Main {
             numbers.add(i);
         }
         return numbers;
-    }
-
-    public static List<Integer> filterEvenNumbers(List<Integer> lista) {
-        List<Integer> even = new ArrayList<>();
-        for(Integer i : lista) {
-            if (i % 2 == 0) {
-                even.add(i);
-            }
-        }
-        return even;
-    }
-
-    public static List<Integer> filterOddNumbers(List<Integer> lista1) {
-        List<Integer> odd = new ArrayList<>();
-        for(Integer i : lista1) {
-            if (i % 2 != 0) {
-                odd.add(i);
-            }
-        }
-        return odd;
     }
 }
